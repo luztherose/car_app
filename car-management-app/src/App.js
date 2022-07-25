00c998fa8e71b1ce0ae92216522500276d5bf8cb
Luz De La Rosa <luzdelarosaramirez@gmail.com>
2022-07-25 17:51:41 -0400

    first commit :rocket:

---
+++ car-management-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Work in Progress!! 🙌🏾
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
